cf2c3bcd30fc37c1e23e38f8570318c7d6eddc2a
IvaGoluza <ig53401@fer.hr>
2024-12-19 11:41:15 +0100

    add glow effect for level button

--- src/components/level-button.tsx
+++ src/components/level-button.tsx
@@ -8,10 +8,10 @@ export default function LevelButton({ level, status }: LevelButtonProps) {
   return (
     <div className="relative">
       <button
-        className={`w-24 h-24 bg-yellow-300 rounded-full flex items-center justify-center border-[#BCFDC6] border-4
-      text-5xl font-bold uppercase text-black drop-shadow-lg font-alumni 
-      ${status !== "ACTIVE" && "brightness-50"} hover:border-lime-400`}
         disabled={status !== "ACTIVE"}
+        className={`w-24 h-24 bg-yellow-300 rounded-full flex items-center justify-center border-[#BCFDC6] border-4
+        text-5xl font-bold uppercase text-black drop-shadow-lg font-alumni hover:border-lime-400
+        ${status === "ACTIVE" ? "shadow-circle shadow-amber-50 border-2 border-amber-50" : "brightness-50"}`}
       >
         {level}
       </button>
