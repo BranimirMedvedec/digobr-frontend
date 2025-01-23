bf4e96e6b468f5ebefe609af635433b1a29a2f0e
IvaGoluza <ig53401@fer.hr>
2025-01-23 13:56:59 +0100

    add competition name

--- src/components/start-competition-button.tsx
+++ src/components/start-competition-button.tsx
@@ -1,92 +1,92 @@
 import {
-	AlertDialog,
-	AlertDialogAction,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTitle,
-	AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Button } from "./ui/button"
-import { startCompetition } from "@/lib/functions"
-import { useNavigate } from "react-router-dom"
-import { useToast } from "@/hooks/use-toast"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { startCompetition } from "@/lib/functions";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
 
 type StartCompetitionButtonProps = {
-	username: string
-	id: number
-	competitionName: string
-}
+  username: string;
+  id: number;
+  competitionName: string;
+};
 
 export default function StartCompetitionButton({
-	username,
-	id,
-	competitionName,
+  username,
+  id,
+  competitionName,
 }: StartCompetitionButtonProps) {
-	const navigate = useNavigate()
-	const { toast } = useToast()
+  const navigate = useNavigate();
+  const { toast } = useToast();
 
-	async function handleStartCompetition() {
-		try {
-			if (!username) {
-				toast({
-					title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
-					variant: "destructive",
-					className: "bg-black text-white border-1 rounded-xl",
-					duration: 2500,
-				})
-				return
-			}
+  async function handleStartCompetition() {
+    try {
+      if (!username) {
+        toast({
+          title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
+          variant: "destructive",
+          className: "bg-black text-white border-1 rounded-xl",
+          duration: 2500,
+        });
+        return;
+      }
 
-			if (id == 0) {
-				toast({
-					title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
-					variant: "destructive",
-					className: "bg-black text-white border-1 rounded-xl",
-					duration: 2500,
-				})
-				return
-			}
+      if (id == 0) {
+        toast({
+          title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
+          variant: "destructive",
+          className: "bg-black text-white border-1 rounded-xl",
+          duration: 2500,
+        });
+        return;
+      }
 
-			await startCompetition(username, id)
-			navigate(`/teacher/competition/${id}`, {
-				state: { competitionName },
-			})
-		} catch (error) {
-			console.log("Error starting competition: ", error)
-		}
-	}
+      await startCompetition(username, id);
+      navigate(`/teacher/competition/${id}`, {
+        state: { name: competitionName },
+      });
+    } catch (error) {
+      console.log("Error starting competition: ", error);
+    }
+  }
 
-	return (
-		<AlertDialog>
-			<AlertDialogTrigger asChild>
-				<Button className="h-12 border-4 border-white rounded-xl text-white uppercase text-3xl font-alumni font-semibold shadow-lg">
-					Pokreni
-				</Button>
-			</AlertDialogTrigger>
-			<AlertDialogContent className="bg-gradient-to-b to-[hsla(113,96%,81%,1)] from-[hsla(188,90%,51%,1)] text-black rounded-xl w-3/4">
-				<AlertDialogHeader>
-					<AlertDialogTitle>
-						Jeste li sigurni da želite pokrenuti natjecanje?
-					</AlertDialogTitle>
-					<AlertDialogDescription>
-						Učenici neće moći uću u grupe nakon što natjecanje
-						počne!
-					</AlertDialogDescription>
-				</AlertDialogHeader>
-				<AlertDialogFooter>
-					<AlertDialogCancel className="bg-white text-black font-hammersmith font-medium text-lg rounded-xl border-2 border-white p-5 shadow-lg">
-						Odustani
-					</AlertDialogCancel>
-					<AlertDialogAction
-						onClick={handleStartCompetition}
-						className="bg-fuchsia-400 text-black font-hammersmith font-medium text-lg rounded-xl border-2 border-white p-5 shadow-lg">
-						Nastavi
-					</AlertDialogAction>
-				</AlertDialogFooter>
-			</AlertDialogContent>
-		</AlertDialog>
-	)
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="h-12 border-4 border-white rounded-xl text-white uppercase text-3xl font-alumni font-semibold shadow-lg">
+          Pokreni
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-gradient-to-b to-[hsla(113,96%,81%,1)] from-[hsla(188,90%,51%,1)] text-black rounded-xl w-3/4">
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Jeste li sigurni da želite pokrenuti natjecanje?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Učenici neće moći uću u grupe nakon što natjecanje počne!
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="bg-white text-black font-hammersmith font-medium text-lg rounded-xl border-2 border-white p-5 shadow-lg">
+            Odustani
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleStartCompetition}
+            className="bg-fuchsia-400 text-black font-hammersmith font-medium text-lg rounded-xl border-2 border-white p-5 shadow-lg"
+          >
+            Nastavi
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
--- src/pages/teacher/CurrentCompetition.tsx
+++ src/pages/teacher/CurrentCompetition.tsx
@@ -1,93 +1,78 @@
-import Title from "@/components/title.tsx"
-import LeaderboardColumn from "@/components/leaderboard-column.tsx"
-import LeaderboardRow from "@/components/leaderboard-row.tsx"
-import { Group, GroupWithPlace } from "@/models/group.ts"
-import { useEffect, useState } from "react"
-import { getCompetition } from "@/lib/functions.ts"
-import { getUsername } from "@/lib/auth-functions.ts"
-import { useLocation, useParams } from "react-router-dom"
-import { useToast } from "@/hooks/use-toast"
+import Title from "@/components/title.tsx";
+import LeaderboardColumn from "@/components/leaderboard-column.tsx";
+import LeaderboardRow from "@/components/leaderboard-row.tsx";
+import { Group, GroupWithPlace } from "@/models/group.ts";
+import { useEffect, useState } from "react";
+import { getCompetition } from "@/lib/functions.ts";
+import { getUsername } from "@/lib/auth-functions.ts";
+import { useLocation, useParams } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
 
 export default function CurrentCompetition() {
-	const { id } = useParams()
-	const location = useLocation()
-	const name = location.state.name
-	const [groups, setGroups] = useState<Group[]>()
-	const [firstThreeGroups, setFirstThreeGroups] = useState<GroupWithPlace[]>()
-	const { toast } = useToast()
+  const { id } = useParams();
+  const location = useLocation();
+  const name = location.state.name;
+  const [groups, setGroups] = useState<Group[]>();
+  const [firstThreeGroups, setFirstThreeGroups] = useState<GroupWithPlace[]>();
+  const { toast } = useToast();
 
-	useEffect(() => {
-		const username = getUsername()
-		if (!username) {
-			toast({
-				title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
-				variant: "destructive",
-				className: "bg-black text-white border-1 rounded-xl",
-				duration: 2500,
-			})
-			return
-		}
+  useEffect(() => {
+    const username = getUsername();
+    if (!username) {
+      toast({
+        title: "Morate biti prijavljeni da biste vidjeli aktivno natjecanje.",
+        variant: "destructive",
+        className: "bg-black text-white border-1 rounded-xl",
+        duration: 2500,
+      });
+      return;
+    }
 
-		const fetchCompetition = () => {
-			getCompetition(username, Number(id))
-				.then((groups) => {
-					const sortedGroups = groups.sort(
-						(a, b) => b.score - a.score
-					)
-					setGroups(sortedGroups)
-					setFirstThreeGroups([
-						{ ...sortedGroups[1], place: 2 },
-						{ ...sortedGroups[0], place: 1 },
-						{ ...sortedGroups[2], place: 3 },
-					])
-				})
-				.catch((error) => {
-					console.error(
-						"Error fetching current competition groups.",
-						error
-					)
-				})
-		}
+    const fetchCompetition = () => {
+      getCompetition(username, Number(id))
+        .then((groups) => {
+          const sortedGroups = groups.sort((a, b) => b.score - a.score);
+          setGroups(sortedGroups);
+          setFirstThreeGroups([
+            { ...sortedGroups[1], place: 2 },
+            { ...sortedGroups[0], place: 1 },
+            { ...sortedGroups[2], place: 3 },
+          ]);
+        })
+        .catch((error) => {
+          console.error("Error fetching current competition groups.", error);
+        });
+    };
 
-		// Initial fetch
-		fetchCompetition()
+    // Initial fetch
+    fetchCompetition();
 
-		// Set up the interval to fetch data every minute
-		const interval = setInterval(fetchCompetition, 60000)
+    // Set up the interval to fetch data every minute
+    const interval = setInterval(fetchCompetition, 60000);
 
-		return () => clearInterval(interval)
-	}, [])
+    return () => clearInterval(interval);
+  }, []);
 
-	return (
-		<div className="min-h-screen w-screen flex flex-col items-center justify-start">
-			<div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full md:pl-6 md:pr-10">
-				<Title
-					showSmallTitle={true}
-					showFrog={false}
-				/>
-				<p className="uppercase font-bold text-[#9775FF] font-alumni text-3xl">
-					{name}
-				</p>
-			</div>
-			<div className="w-full px-10 flex flex-col md:flex-row gap-5">
-				<div className="flex items-end gap-2 md:gap-4 h-[47vh] mt-10 md:mt-0 md:h-[78vh] w-full md:w-2/3">
-					{firstThreeGroups?.map((group: GroupWithPlace, index) => (
-						<LeaderboardColumn
-							key={index}
-							{...group}
-						/> // Spread the group object directly
-					))}
-				</div>
-				<div className="flex flex-col h-[78vh] md:overflow-scroll w-full md:w-1/3 gap-2 mb-20 md:mb-0">
-					{groups?.slice(3).map((group, index) => (
-						<LeaderboardRow
-							key={group.code}
-							place={index + 4}
-							{...group}
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="min-h-screen w-screen flex flex-col items-center justify-start">
+      <div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full md:pl-6 md:pr-10">
+        <Title showSmallTitle={true} showFrog={false} />
+        <p className="uppercase font-bold text-[#9775FF] font-alumni text-3xl">
+          {name}
+        </p>
+      </div>
+      <div className="w-full px-10 flex flex-col md:flex-row gap-5">
+        <div className="flex items-end gap-2 md:gap-4 h-[47vh] mt-10 md:mt-0 md:h-[78vh] w-full md:w-2/3">
+          {firstThreeGroups?.map((group: GroupWithPlace, index) => (
+            <LeaderboardColumn key={index} {...group} /> // Spread the group object directly
+          ))}
+        </div>
+        <div className="flex flex-col h-[78vh] md:overflow-scroll w-full md:w-1/3 gap-2 mb-20 md:mb-0">
+          {groups?.slice(3).map((group, index) => (
+            <LeaderboardRow key={group.code} place={index + 4} {...group} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
