0c22a3f31ecc6950d238db0046f57c5a41b64696
BranimirMedvedec <94074954+BranimirMedvedec@users.noreply.github.com>
2025-01-24 10:20:33 +0100

    update current competition style

--- src/components/leaderboard-row.tsx
+++ src/components/leaderboard-row.tsx
@@ -1,23 +1,22 @@
-import LeaderboardScore from "@/components/leaderboard-score.tsx";
-import { GroupWithPlace } from "@/models/group.ts";
+import LeaderboardScore from "@/components/leaderboard-score.tsx"
+import { GroupWithPlace } from "@/models/group.ts"
 
 export default function LeaderboardRow(group: GroupWithPlace) {
-  return (
-    <div className="mx-2 md:mx-4 lg:mx-16 px-6 py-3 bg-white bg-opacity-60 rounded-2xl flex items-center justify-between border-white border-2">
-      <div className="bg-white text-black font-extrabold font-alumni text-lg md:text-3xl px-2 md:px-4 md:py-2 rounded md:rounded-xl">
-        {group.place}
-      </div>
-      <div
-        className="relative py-2 h-10 md:h-full w-1/3 border-[3px] border-white rounded-xl"
-        style={{
-          backgroundColor: group.colorRgb,
-        }}
-      >
-        <div className="absolute -bottom-1 -right-4 bg-white px-2 font-bold font-alumni text-xl md:text-2xl rounded-2xl">
-          {group.code}
-        </div>
-      </div>
-      <LeaderboardScore score={group.score} />
-    </div>
-  );
+	return (
+		<div className="mx-2 md:mx-4 lg:mx-16 px-6 py-3 bg-white bg-opacity-60 rounded-2xl flex items-center justify-between border-white border-2">
+			<div className="bg-white text-black font-extrabold font-alumni text-lg md:text-3xl px-2 md:px-4 md:py-2 rounded md:rounded-xl">
+				{group.place}
+			</div>
+			<div
+				className="relative py-2 h-10 md:h-full w-1/3 border-[3px] border-white rounded-xl"
+				style={{
+					backgroundColor: group.colorRgb,
+				}}>
+				<div className="absolute -bottom-1 -right-4 bg-white px-2 font-bold font-alumni text-xl md:text-2xl rounded-2xl">
+					{group.code}
+				</div>
+			</div>
+			<LeaderboardScore score={group.score} />
+		</div>
+	)
 }
